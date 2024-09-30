bae6204a965ff4f480a9604058826164e604d847
RobsonOliveiraSouza <robson.oliveira.souza21@gmail.com>
2024-09-30 11:26:25 -0300

    first commit

---
+++ src/main/java/br/com/robsonsouza/todolist/entity/Todo.java
@@ -0,0 +1,17 @@
+package br.com.robsonsouza.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "todos")
+@Data
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+}
---
+++ src/test/java/br/com/robsonsouza/todolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robsonsouza.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
